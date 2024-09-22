3bd948e87e6aa2c33c7370e06fb3b36e5e1decfc
nathalieoliekan <nathalie.oliekan@gmail.com>
2024-09-21 18:19:53 -0700

    Create code to calculate sum, average, and max of a list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers_list = new List<int>();
+        int number = 1; // Doesn't matter what number is as long as it is defined. Code will run at least once and get user input
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string answer = Console.ReadLine();
+            number = int.Parse(answer);
+
+            if (number != 0)
+            {
+                numbers_list.Add(number);
+            }
+
+        }while (number != 0);
+
+        int sum = 0;
+        foreach (int num in numbers_list)
+        {
+            sum += num; // add each item in list together to get sum
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float total = sum; // convert sum to float for average calculation
+
+        float average = total / numbers_list.Count; // Calculate the average
+        Console.WriteLine($"The average is: {average}");
+
+        int max = 0;
+        foreach(int num in numbers_list)
+        {
+            if (num > max)
+            {
+                max = num; 
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
